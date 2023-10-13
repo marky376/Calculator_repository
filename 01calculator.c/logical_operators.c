2714fea268b755532767f073041b17e49ac9fd97
marky376 <mmutuamark@gmail.com>
2023-10-13 05:23:50 +0300

    A commit on logical operators && bitwise operators.

--- 01calculator.c/logical_operators.c
+++ 01calculator.c/logical_operators.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int main(void)
 {
 	int i, j;
@@ -12,6 +12,30 @@ int main(void)
 
 	printf("Logical and is: %d\n", i && j);
 	printf("Logical OR is: %d\n", i || j);
-	printf("Logical exclusive OR is: %d\n", i ^ j);
+	printf("Logical NOT is: %d\n", !(i && j));
 	return (0);
+}
+*/
+
+int main(void)
+{
+	int i, j;
+
+	printf("Enter number: ");
+	scanf("%d", &i);
+
+	printf("Enter another number: ");
+	scanf("%d", &j);
+
+	printf("Bitwise and of the two numbers is: %d\n", i & j);
+	printf("Bitwise or of the two numbers is: %d\n", i | j);
+	printf("Bitwise exclusive OR is: %d\n", i ^ j);
+	printf("Bitwise binary left operator is: %d\n", i << j);
+	printf("Bitwise binary right operator is: %d\n", i >> j);
+	printf("Bitwise ones complement of the first number is: %d\n", ~i);
+	printf("Bitwise ones compliment of the second number is: %d\n", ~j);
+
+
+
+	return 0;
 }
