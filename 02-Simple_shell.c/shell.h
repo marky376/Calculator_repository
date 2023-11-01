9b6e9d0565cdcc3f1665dc277e24fe46b5138171
marky376 <mmutuamark@gmail.com>
2023-11-01 15:47:07 +0300

    A commit on a sample simple shell. Bet you didn't C this coming.

--- 02-Simple_shell.c/shell.h
+++ 02-Simple_shell.c/shell.h
@@ -9,5 +9,6 @@
 void display_prompt(void);
 void markm_print(const char *format, ...);
 void execute_command(const char *command) __attribute__((unused));
+void read_command(char *commnad(char *command, size_t size);
 
 #endif
