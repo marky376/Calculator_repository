e1015285a475fa2baa4079e21b8808936dfdb50a
marky376 <mmutuamark@gmail.com>
2024-01-22 11:37:44 +0300

    A commit on some files to work on

--- 01calculator.c/alg2.c
+++ 01calculator.c/alg2.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-	char name[] = {"Joseph", "Mark"};
-	char number[] = {+254 678543, +254 098223};
+	char name[5] = {"Joseph", "Mark"};
+	char number[5] = {+254 678543, +254 098223};
 
 	for(int i = 0; i < 4; i++)
 	{
-		strcmp
+		strcmp(
 	}
 }
