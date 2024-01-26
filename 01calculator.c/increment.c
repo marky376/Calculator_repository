a441299c2381b31dee3b180d6c571fd30048e36c
marky376 <mmutuamark@gmail.com>
2024-01-26 06:45:46 +0300

    A commit on some C files

---
+++ 01calculator.c/increment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int c = 4;
+
+	printf("%d\n", c);
+	printf("%d\n", c++);/* post-incrementing
+
+
+	printf("%d\n", c);
+
+	printf("%d\n", ++c);
+
+	return (0);
+}
