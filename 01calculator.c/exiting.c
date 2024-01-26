6b04908e005e8e0b5f264229a67ba23f9d1de2aa
marky376 <mmutuamark@gmail.com>
2024-01-26 07:12:18 +0300

    A commit on some C files

---
+++ 01calculator.c/exiting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+        int c = 4;
+
+        printf("%d\n", c);
+        printf("%d\n", c++);/* post-incrementing */
+
+
+        printf("%d\n", c);
+	
+	exit;
+        printf("%d\n", ++c);/* pre-incrementing */
+
+}
