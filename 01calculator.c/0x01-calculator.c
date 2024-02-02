c1b2e7e08f56de253c12292efbeac38adec8c5f5
marky376 <mmutuamark@gmail.com>
2024-02-02 17:45:38 +0300

    Adding comments on files

--- 01calculator.c/0x01-calculator.c
+++ 01calculator.c/0x01-calculator.c
@@ -2,40 +2,39 @@
 
 int main(void)
 {
-	char operator;
-	double num1, num2;
+	char operator; // Variable to store the operator
+	double num1, num2; // Variables to store the numbers
 
 	printf("Enter an operator (Addition\nSubtraction\nMultiplication\nDivision\nModulos\n");
-	scanf("%c", &operator);
+	scanf("%c", &operator); // Read the operator from the user
 
 	printf("Enter two numbers: ");
-	scanf("%lf %lf", &num1, &num2);
+	scanf("%lf %lf", &num1, &num2); // Read the two numbers from the user
 
 	switch (operator)
 	{
 		case 'Addition':
 			printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
 			break;
-		case 'Subtraction':
+		case '-': // If the operator is Subtraction
 			printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
 			break;
-
-		case 'Multiplication':
+		case '*': // If the operator is Multiplication
 			printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
 			break;
-
-		case 'Division':
+			printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+			break;
+		case 'Division': // If the operator is Division
 			if (num2 != 0)
-			{	
+			{   
 				printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
 			}
 			else
 			{
 				printf("Division by zero is not allowed,\n");
-
 			}
 			break;
-		case 'Modulo':
+		case 'Modulo': // If the operator is Modulo
 			if (num1 > num2)
 			{
 				printf("The modulus of %.2lf is %2.lf\n", num1, num1 % num2);
@@ -45,11 +44,14 @@ int main(void)
 				printf("num2 should be less than num1: ");
 				break;
 			}	
+			}	
+
+
+			}   
 
 
 		default :
-			printf("invalid operator.\n");
-
+			printf("invalid operator.\n"); // If the operator is invalid
 	}
 
 	return 0;
--- 01calculator.c/10_timesdo_while.c
+++ 01calculator.c/10_timesdo_while.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * This program calculates the multiplication of a number with 10 using a do-while loop.
+ * It starts with n = 0 and continues until the result (j) is less than 100.
+ * The value of j is printed after each iteration.
+ */
+
 int main(void)
 {
 	int n, m, j;
@@ -13,6 +19,7 @@ int main(void)
 		n++;
 		printf("%d\n", j);
 
-	}while (j < 100);
+	} while (j < 100);
+
 	return 0;
 }
