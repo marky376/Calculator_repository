3b67ff26d519c6073767aff7482ecf926e266d24
marky376 <mmutuamark@gmail.com>
2024-02-02 17:52:10 +0300

    Adding comments on files

--- 01calculator.c/area_of_square.c
+++ 01calculator.c/area_of_square.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-	int a;
+	int a; // variable to store the length of a square
 
 	printf("Enter the value of the length of a square: ");
-	scanf("%d", &a);
+	scanf("%d", &a); // read the length from the user
 
-	printf("The area of the given square is: %d", a * a);
+	printf("The area of the given square is: %d", a * a); // calculate and print the area
 
 	return (0);
 }
