e532edb653917af7280cecccd4260b2bddf9787b
marky376 <mmutuamark@gmail.com>
2024-02-02 17:58:31 +0300

    Adding comments on files

--- 01calculator.c/case_proj.c
+++ 01calculator.c/case_proj.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * This program takes input from the user, which represents a student's marks.
+ * It then uses a switch statement to determine the grade based on the input.
+ * The program prints the corresponding grade based on the following ranges:
+ * - 0 to 39: 'E' grade
+ * - 40 to 49: 'D' grade
+ * - 50 to 59: 'C' grade
+ * - 60 to 69: 'B' grade
+ * - 70 to 100: 'A' grade
+ * If the input is not within the valid range, an error message is displayed.
+ */
+
 int main(void)
 {
 	int n;
@@ -25,9 +37,8 @@ int main(void)
 			printf("The student has achieved an 'A' grade");
 			break;
 		default:
-			printf("Invalid entry.Please enter a value that is between 0 to 100");
+			printf("Invalid entry. Please enter a value that is between 0 to 100");
 	}
 
 	return (0);
 }
-
