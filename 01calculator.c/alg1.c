3ecea3780ac263139c6331223edc7a650a269120
marky376 <mmutuamark@gmail.com>
2024-02-02 17:47:09 +0300

    Adding comments on files

--- 01calculator.c/alg1.c
+++ 01calculator.c/alg1.c
@@ -3,20 +3,22 @@
 
 int main(void)
 {
-
-	int numbers[] = {4, 6, 8, 2, 7,5 ,0};
+	// Define an array of numbers
+	int numbers[] = {4, 6, 8, 2, 7, 5, 0};
 	
+	// Iterate through the array
 	for (int i = 0; i < 7; i++)
 	{
+		// Check if the current number is -1
 		if (numbers[i] == -1)
 		{
+			// If -1 is found, print "Found" and return 0
 			printf("Found\n");
 			return (0);
 		}
-
 	}
 	
+	// If -1 is not found, print "Not found" and return 1
 	printf("Not found\n");
 	return (1);
-
 }
