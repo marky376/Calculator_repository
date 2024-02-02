9cc5b37e5d9c9a5d8b5091d05b46a8913664ad66
marky376 <mmutuamark@gmail.com>
2024-02-02 17:50:34 +0300

    Adding comments on files

--- 01calculator.c/area_of_circle.c
+++ 01calculator.c/area_of_circle.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * Calculates the area of a circle.
+ * 
+ * This program prompts the user to enter the radius of a circle,
+ * then calculates and prints the area of the circle using the formula:
+ * area = pi * radius * radius.
+ * 
+ * @return 0 indicating successful execution of the program.
+ */
 int main(void)
 {
 	int radius;
@@ -10,7 +19,6 @@ int main(void)
 	printf("Enter the radius of the circle: ");
 	scanf("%d", &radius);
 
-
 	printf("The area of the given circle is: %f", pi * radius * radius);
-	return (0);
+	return 0;
 }
