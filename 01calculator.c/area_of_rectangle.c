53e68ddb1d1246a031c79a3a49e9bdf3863e80cd
marky376 <mmutuamark@gmail.com>
2024-02-02 17:51:30 +0300

    Adding comments on files

--- 01calculator.c/area_of_rectangle.c
+++ 01calculator.c/area_of_rectangle.c
@@ -4,12 +4,15 @@ int main(void)
 {
 	int a, b;
 
+	// Prompt the user to enter the length of the rectangle
 	printf("Enter the size of the length: ");
 	scanf("%d", &a);
 
+	// Prompt the user to enter the width of the rectangle
 	printf("Enter the size of the width: ");
 	scanf("%d", &b);
 
+	// Calculate and print the area of the rectangle
 	printf("The area of the rectangle is: %d", a * b);
 
 	return (0);
