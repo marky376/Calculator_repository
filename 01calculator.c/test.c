05671b22099d949ead416b1454af48d983952742
marky376 <mmutuamark@gmail.com>
2024-02-03 16:23:48 +0300

    Adding comments on files

--- 01calculator.c/test.c
+++ 01calculator.c/test.c
@@ -4,16 +4,16 @@ int main()
 {
 	int i;
 
-	printf("Enter a number: ");
-	scanf("%d", &i);
+	printf("Enter a number: "); // Prompt the user to enter a number
+	scanf("%d", &i); // Read the input number from the user
 
-	if (i % 2 == 0)
+	if (i % 2 == 0) // Check if the number is even
 	{
-		printf("Even number\n");
+		printf("Even number\n"); // Print "Even number" if the number is even
 	}
 	else
 	{
-		printf("Odd number\n");
+		printf("Odd number\n"); // Print "Odd number" if the number is odd
 	}
 
 	return (0);
