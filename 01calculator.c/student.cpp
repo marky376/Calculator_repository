1857709d521ba03c8c629efc2d3344e6c2d5f1d0
marky376 <mmutuamark@gmail.com>
2024-02-03 16:21:20 +0300

    Adding comments on files

--- 01calculator.c/student.cpp
+++ 01calculator.c/student.cpp
@@ -9,27 +9,28 @@ int main(void)
 	int sum = 0;
 	int i;
 
+	// Prompt the user to enter the marks for each student
 	for (i = 0; i < numStudents; ++i)
 	{
-		cout << "Enter students marks " << i + 1 << " : ";
+		cout << "Enter student's marks " << i + 1 << ": ";
 		cin >> marks[i];
-	
 
-
+		// Validate the input to ensure it is a non-negative number
 		while (marks[i] < 0)
 		{
-			cout << "Invalid entry please enter a valid number ";
+			cout << "Invalid entry. Please enter a valid number: ";
 			cin >> marks[i];
 		}
 
-	
+		// Calculate the sum of all the marks
+		sum += marks[i];
 	}
 
-	sum += marks[i];
+	// Calculate the average marks
+	double average = static_cast<double>(sum) / numStudents;
 
-	double average = (sum) / numStudents;
-
-	cout << "Average marks of " << numStudents << " is " << average << endl;
+	// Display the average marks
+	cout << "Average marks of " << numStudents << " students is " << average << endl;
 
 	return 0;	
 }
