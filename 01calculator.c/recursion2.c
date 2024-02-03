384e2f6c8a91f35b3c7c15c147f49f76b931148a
marky376 <mmutuamark@gmail.com>
2024-02-03 16:18:47 +0300

    Adding comments on files

--- 01calculator.c/recursion2.c
+++ 01calculator.c/recursion2.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
+// Function to recursively print numbers from nb to 0
 int print(int nb)
 {
+    // Base case: if nb is negative, return 0
     if (nb < 0) 
     {
         return (0);
     }
+    
+    // Print the current number and recursively call print() with nb-1
     printf("%d", nb + print(nb - 1));
-    nb --;
+    
+    // Decrement nb
+    nb--;
+    
+    // Return the decremented nb
     return (nb);
 }
 
+// Main function
 int main(void)
 {
+    // Call the print() function with argument 4
     print(4);
+    
+    // Return 0 to indicate successful execution
     return (0);
 }
