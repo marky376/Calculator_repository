4f37428150849898308ff2f5ff0b898e59f007a3
marky376 <mmutuamark@gmail.com>
2024-02-03 16:13:00 +0300

    Adding comments on files

--- 01calculator.c/positive_inbetweenzero_and_50.c
+++ 01calculator.c/positive_inbetweenzero_and_50.c
@@ -4,11 +4,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 50; i++)
+	for (i = 0; i <= 50; i++) // Iterate from 0 to 50 (inclusive)
 	{
-		if (i % 2 == 0)
+		if (i % 2 == 0) // Check if the number is even
 		{
-			printf("%d", i);
+			printf("%d", i); // Print the even number
 		}
 				
 	}
