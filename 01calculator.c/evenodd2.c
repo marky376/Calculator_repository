c80ad09581a1ccbd8d00d3ed6295ecc4e6c9d5a4
marky376 <mmutuamark@gmail.com>
2024-02-03 15:55:05 +0300

    Adding comments on files

--- 01calculator.c/evenodd2.c
+++ 01calculator.c/evenodd2.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-
+// Function to check if a number is even or odd
 int evenoddchecker(int n)
 {
+	// Validate input: ensure n is greater than zero
 	while (n < 0)
 	{
 		printf("Please enter a number which is greater than zero(0) ");
 		scanf("%d", &n);
 	}
+	// Validate input: ensure n is not zero
 	while (n == 0)
 	{
 		printf("Please enter a number which is greater than zero(0) ");
 		scanf("%d", &n);
 	}
 
+	// Check if n is even or odd
 	if (n % 2 == 0)
 	{
 		printf("%d is an even number", n);
@@ -24,7 +27,6 @@ int evenoddchecker(int n)
 	}
 }
 
-
 int main()
 {
 	int n, check;
@@ -32,8 +34,8 @@ int main()
 	printf("Enter any number: ");
 	scanf("%d", &n);
 
+	// Call the evenoddchecker function
 	check = evenoddchecker(n);
 
 	return 0;
 }
-
