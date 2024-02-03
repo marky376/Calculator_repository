1f50d049b2ae2afc72d4461d3c0395d47f9a7f0c
marky376 <mmutuamark@gmail.com>
2024-02-03 16:16:17 +0300

    Adding comments on files

--- 01calculator.c/prtea4.c
+++ 01calculator.c/prtea4.c
@@ -5,20 +5,20 @@ int main()
 	int m, b, row, col, k;
 	char h = '*';
 
-	row = 4;
-	col = 4;
+	row = 4; // Number of rows in the pattern
+	col = 4; // Number of columns in the pattern
 
-	for (m = 1; m <= row; m++)
+	for (m = 1; m <= row; m++) // Loop for each row
 	{
-		for (b = 1; b <= row - m; b++)
+		for (b = 1; b <= row - m; b++) // Loop for printing spaces before the pattern
 		{
 			printf(" ");
 		}
-		for (k = 1; k <= 2 * m - 1; k++)
+		for (k = 1; k <= 2 * m - 1; k++) // Loop for printing the pattern
 		{
 			printf("%c", '*');
 		}
-		printf("\n");
+		printf("\n"); // Move to the next line after printing each row
 	}
 	
 	return 0;
