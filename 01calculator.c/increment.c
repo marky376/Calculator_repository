e7341fcff03a52df37e29e8639a448f2e8144762
marky376 <mmutuamark@gmail.com>
2024-02-03 16:03:09 +0300

    Adding comments on files

--- 01calculator.c/increment.c
+++ 01calculator.c/increment.c
@@ -4,13 +4,15 @@ int main(void)
 {
 	int c = 4;
 
-	printf("%d\n", c);
-	printf("%d\n", c++);/* post-incrementing */
+	printf("%d\n", c); // Print the initial value of c
 
+	printf("%d\n", c++); /* Post-incrementing: 
+	                       Print the value of c and then increment it */
 
-	printf("%d\n", c);
+	printf("%d\n", c); // Print the updated value of c
 
-	printf("%d\n", ++c);/* pre-incrementing */
+	printf("%d\n", ++c); /* Pre-incrementing: 
+	                       Increment the value of c and then print it */
 
 	return (0);
 }
