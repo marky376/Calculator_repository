a083c396adaadb075a963a0da9d6ec8ffe7386d4
marky376 <mmutuamark@gmail.com>
2024-02-03 16:25:49 +0300

    Adding comments on files

--- 01calculator.c/twodimension.c
+++ 01calculator.c/twodimension.c
@@ -2,17 +2,18 @@
 
 int main(void)
 {
-	/*array of 5 rows and 2 columns*/
-	int a [5] [2] = {{0,0} , {1,2} , {2,4}, {3,6}, {4,8}};
+	/* Define a 2D array with 5 rows and 2 columns */
+	int a[5][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
 	int i, j;
-	int pick = a[2][1];
+	int pick = a[2][1]; // Pick the value at row 2, column 1
 
-	/*output each value in the 2D array*/
+	/* Output each value in the 2D array */
 	for (i = 0; i < 5; i++)
 	{
 		for (j = 0; j < 2; j++)
-			printf("a [%d] [%d]: %d\n", i, j, a [i] [j]);
+			printf("a[%d][%d]: %d\n", i, j, a[i][j]);
 	}
-	printf("%d\n", pick);
+
+	printf("%d\n", pick); // Print the picked value
 	return 0;
 }
