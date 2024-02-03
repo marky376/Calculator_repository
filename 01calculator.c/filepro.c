f48952d82f9e938d1ea71cfbb6d960b5bf6ad253
marky376 <mmutuamark@gmail.com>
2024-02-03 15:57:30 +0300

    Adding comments on files

--- 01calculator.c/filepro.c
+++ 01calculator.c/filepro.c
@@ -1,74 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-/*
-int main(void)
-{
-	FILE* mercy;
 
-	mercy = fopen("mercy.txt", "w");
-	fprintf(mercy, "%s", "Fantastic stuff you're doing there at the University Of Embu");
-
-	fclose(mercy);
-
-	return (0);
-}*/
-
-/*
-int main(void)
-{
-    FILE* mercy;
-    char text[] = "Fantastic stuff you're doing there at the University Of Nairobi";
-
-    mercy = fopen("mercy.txt", "w");
-
-    if (mercy == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-
-    fputs(text, mercy);
-
-    fclose(mercy);
-
-    return 0;
-}
-*/
-
-
-int main(void)
-{
-	FILE* mercy;
-	char text[1000];
-
-	mercy = fopen("mercy.txt", "r");
-	printf("%s", fgets(text, 100, mercy));
-
-	fclose(mercy);
-
-	return (0);
-}
-
-#include <stdio.h>
-/*
 int main(void)
 {
     FILE* mercy;
     char text[1000];
 
-    mercy = fopen("mercy.txt", "r");
-    if (mercy == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
+    mercy = fopen("mercy.txt", "r"); // Open the file "mercy.txt" in read mode
+    printf("%s", fgets(text, 100, mercy)); // Read the first 100 characters from the file and print them
 
-    if (fgets(text, sizeof(text), mercy) != NULL) {
-        printf("%s", text);
-    } else {
-        printf("Error reading from file.\n");
-    }
+    fclose(mercy); // Close the file
 
-    fclose(mercy);
-
-    return 0;
-}*/
-
+    return (0);
+}
