c55cacf7369522c2c050c4d538f2d3ba1cf96fba
marky376 <mmutuamark@gmail.com>
2024-02-03 16:18:06 +0300

    Adding comments on files

--- 01calculator.c/recursion.c
+++ 01calculator.c/recursion.c
@@ -1,10 +1,18 @@
-#include <unistd.h>
-
+/**
+ * @brief Prints a single character to the standard output.
+ * 
+ * @param c The character to be printed.
+ */
 void print_char(char c)
 {
 	write(1, &c, 1);
 }
 
+/**
+ * @brief Recursively prints all lowercase letters starting from the given character.
+ * 
+ * @param c The starting character.
+ */
 void print_all_letters_starting_from(char c)
 {
 	if (c > 'z')
@@ -14,10 +22,20 @@ void print_all_letters_starting_from(char c)
 	print_char(c);
 	print_all_letters_starting_from(c + 1);
 }
+
+/**
+ * @brief Prints the entire lowercase alphabet.
+ */
 void print_alphabet(void)
 {
 	print_all_letters_starting_from('a');
 }
+
+/**
+ * @brief The entry point of the program.
+ * 
+ * @return 0 on successful execution.
+ */
 int main(void)
 {
 	print_alphabet();
