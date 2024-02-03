2e31d853e5cf80b19702535ddbfdd17911442d9d
marky376 <mmutuamark@gmail.com>
2024-02-03 16:28:23 +0300

    Adding comments on files

--- 02-Simple_shell.c/executing2.c
+++ 02-Simple_shell.c/executing2.c
@@ -1,13 +1,15 @@
-#include "shell.h"
-#include <sys/wait.h>
-
-
+/**
+ * Executes the given command by forking a child process and using execvp to execute the command.
+ * 
+ * @param command The command to be executed.
+ */
 void execute_commands(const char *command/* __attribute__((unused)*/)
 {
 	pid_t child_pid = fork();
 	int status, arg_count = 0;
 	char *tokn, *args[20];
 
+	// Tokenize the command into individual arguments
 	tokn = strtok((char *)command, " ");
 	while (tokn != NULL)
 	{
@@ -18,7 +20,6 @@ void execute_commands(const char *command/* __attribute__((unused)*/)
 
 	child_pid = fork();
 
-
 	if (child_pid == -1)
 	{
 		markm_print("Error forking process.\n");
@@ -26,16 +27,18 @@ void execute_commands(const char *command/* __attribute__((unused)*/)
 	}
 	else if (child_pid == 0)
 	{
+		// Execute the command in the child process
 		if (execvp(args[0], args) == -1)
 		{
 			perror("execvp failed");
 			exit(EXIT_FAILURE);
-
+		}
 	}
 	else
 	{
 		markm_print("Parent process: PID = %d\n", getpid());
 
+		// Wait for the child process to finish
 		wait(&status);
 
 		markm_print("Child process has finished\n");
