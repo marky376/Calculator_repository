a2178596ba7545cd010256d8ae5f1130959a9d81
marky376 <mmutuamark@gmail.com>
2024-02-03 16:19:20 +0300

    Adding comments on files

--- 01calculator.c/strlen.c
+++ 01calculator.c/strlen.c
@@ -3,10 +3,14 @@
 
 int main(void)
 {	
+	// Declare a character array and initialize it with the string "John"
 	char name[] = "John";
+	
+	// Call the strlen() function from the string.h library to get the length of the string
 	int length = strlen(name);
 
+	// Print the length of the string to the console
 	printf("The length of a string is: %d\n", length);
 
 	return 0;
-}	
+}
