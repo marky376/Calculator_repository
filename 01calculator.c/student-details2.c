edbea634ca6ab79f610705ae205d95873ee53412
marky376 <mmutuamark@gmail.com>
2024-02-03 16:20:46 +0300

    Adding comments on files

--- 01calculator.c/student-details2.c
+++ 01calculator.c/student-details2.c
@@ -2,28 +2,29 @@
 
 int main() 
 {	
+	// Declare variables to store student details
 	char studentName[100];
 	char regNumber[100];
 	char lecturerName[100];
 	char course[100];
 
-    /* Prompt for and read student's name*/
+    /* Prompt for and read student's name */
 	printf("Enter student's name: \n");
 	scanf("%99[^\n]%*c", studentName);
 
-    /* Prompt for and read registration number*/
+    /* Prompt for and read registration number */
 	printf("Enter registration number: \n");
 	scanf("%99[^\n]%*c", regNumber);
 
-    /* Prompt for and read lecturer's name*/
+    /* Prompt for and read lecturer's name */
 	printf("Enter lecturer's name: \n");
 	scanf("%99[^\n]%*c", lecturerName);
 
-    /* Prompt for and read course name*/
+    /* Prompt for and read course name */
 	printf("Enter course name: \n");
 	scanf("%99[^\n]%*c", course);
 
-    /* Display the collected information*/
+    /* Display the collected information */
 	printf("\nStudent's Information:\n");
 	printf("Name: %s\n", studentName);
 	printf("Registration Number: %s\n", regNumber);
@@ -32,4 +33,3 @@ int main()
 
 	return 0;
 }
-
