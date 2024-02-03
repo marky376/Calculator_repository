3e2b922481fedc45619685766afcf66611a83e00
marky376 <mmutuamark@gmail.com>
2024-02-03 16:27:43 +0300

    Adding comments on files

--- 02-Simple_shell.c/displaying_prompt.c
+++ 02-Simple_shell.c/displaying_prompt.c
@@ -1,3 +1,9 @@
+/**
+ * @brief Displays the prompt for the shell.
+ * 
+ * This function is responsible for displaying the prompt for the shell.
+ * The prompt is set to "MyShell$ ".
+ */
 #include "shell.h"
 
 void display_prompt(void)
