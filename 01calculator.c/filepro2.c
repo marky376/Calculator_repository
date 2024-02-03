6be2162d1e112239e5eb807105e74d828cc29e30
marky376 <mmutuamark@gmail.com>
2024-02-03 15:58:47 +0300

    Adding comments on files

--- 01calculator.c/filepro2.c
+++ 01calculator.c/filepro2.c
@@ -18,20 +18,21 @@ int main(void)
 
 int main(void)
 {
-	FILE* fptr;
-	char print[1000];
+	FILE* fptr; // File pointer to handle file operations
+	char print[1000]; // Buffer to store each line of the file
 
-	fptr = fopen("fptr.txt", "r");
+	fptr = fopen("fptr.txt", "r"); // Open the file in read mode
 
-	if (fptr != NULL)
+	if (fptr != NULL) // Check if the file was opened successfully
 	{
-		while (fgets(print, sizeof(print), fptr) != NULL)
-			printf("%s", print);
-		fclose(fptr);
+		while (fgets(print, sizeof(print), fptr) != NULL) // Read each line of the file
+			printf("%s", print); // Print the line
+
+		fclose(fptr); // Close the file
 	}
 	else
 	{
-		printf("Error reading the input in the given file!");
+		printf("Error reading the input in the given file!"); // Print an error message if the file cannot be opened
 	}
 
 	return (0);
