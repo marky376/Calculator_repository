1c8f1c2b4498da560f7e7d9549d9e0a79688d625
marky376 <mmutuamark@gmail.com>
2024-02-03 16:14:43 +0300

    Adding comments on files

--- 01calculator.c/prtea.c
+++ 01calculator.c/prtea.c
@@ -4,17 +4,17 @@ int main()
 {
 	int n, row, col;
 
-	char t = '*';
+	char t = '*'; // Initialize a character variable t with '*'
 
-	n = 6;
-	for (row = 1; row <= n; row++)
+	n = 6; // Set the value of n to 6
+	for (row = 1; row <= n; row++) // Loop through each row from 1 to n
 	{
-		for (col = 1; col <= row; col++)
+		for (col = 1; col <= row; col++) // Loop through each column from 1 to row
 		{
-			printf("%c", '*');
-			t++;
+			printf("%c", '*'); // Print the character '*'
+			t++; // Increment the value of t
 		}
-		printf("\n");
+		printf("\n"); // Print a new line after each row
 	}
 
 	return 0;
