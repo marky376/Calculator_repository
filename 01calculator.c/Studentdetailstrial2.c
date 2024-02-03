ba7fc73dc11890cdf15457f4dd5b21956a5d73ed
marky376 <mmutuamark@gmail.com>
2024-02-03 16:22:49 +0300

    Adding comments on files

--- 01calculator.c/Studentdetailstrial2.c
+++ 01calculator.c/Studentdetailstrial2.c
@@ -2,24 +2,24 @@
 
 int main(void)
 {
-	char StudentName[50];
-	char Class[50];
-	char Gender[50];
+	char StudentName[50]; // variable to store student name
+	char Class[50]; // variable to store class
+	char Gender[50]; // variable to store gender
 
 	printf("Please enter your name: ");
-	fgets(StudentName, sizeof(StudentName), stdin);
+	fgets(StudentName, sizeof(StudentName), stdin); // read student name from user
 
-	printf("Please enter your class in  either streams of A, B, C and D: ");
-	fgets(Class, sizeof(Class), stdin);
+	printf("Please enter your class in either streams of A, B, C and D: ");
+	fgets(Class, sizeof(Class), stdin); // read class from user
 
 	printf("Please enter your gender either 'M' for Male and 'F' for Female: ");
-	fgets(Gender, sizeof(Gender), stdin);
+	fgets(Gender, sizeof(Gender), stdin); // read gender from user
 
-	printf("Thanks for your information: %s\n", StudentName);
+	printf("Thanks for your information: %s\n", StudentName); // display student name
 
-	if (Class != NULL)
+	if (Class != NULL) // check if class is not empty
 	{
-		printf("Please seek guidance from the registration office\n");
+		printf("Please seek guidance from the registration office\n"); // display message
 	}
 
 	return 0;
