6909c8b0c3edfbde0cd219ba2d263a38e30f3dc2
marky376 <mmutuamark@gmail.com>
2024-02-03 16:05:32 +0300

    Adding comments on files

--- 01calculator.c/lepyear.c
+++ 01calculator.c/lepyear.c
@@ -4,16 +4,21 @@ int main()
 {
 	int w;
 
-	printf("Please enter the year you wish to enter: ");
+	// Prompt the user to enter a year
+	printf("Please enter the year you wish to check: ");
 	scanf("%d", &w);
 
-	if (w / 4)
+	// Check if the year is divisible by 4
+	if (w % 4 == 0)
 	{
+		// If divisible by 4, it may be a leap year
 		printf("The year %d you entered is a leap year!", w);
 	}
 	else
 	{
+		// If not divisible by 4, it is not a leap year
 		printf("The year %d you entered is not a leap year!", w);
 	}
+
 	return 0;
 }
