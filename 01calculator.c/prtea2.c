8745cd3838ccdd726d7729f7a7d834c8664dfe76
marky376 <mmutuamark@gmail.com>
2024-02-03 16:15:17 +0300

    Adding comments on files

--- 01calculator.c/prtea2.c
+++ 01calculator.c/prtea2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * This program prints a pattern of asterisks in a square shape.
+ * The size of the square is determined by the value of 'n'.
+ */
 int main()
 {
 	int n, row, col;
