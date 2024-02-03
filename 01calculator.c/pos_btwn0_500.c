ee39e1c30a3b6b97a6dd9af294d04a858e2c20a4
marky376 <mmutuamark@gmail.com>
2024-02-03 16:12:26 +0300

    Adding comments on files

--- 01calculator.c/pos_btwn0_500.c
+++ 01calculator.c/pos_btwn0_500.c
@@ -4,9 +4,9 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 500; n +=2)
+	for (n = 0; n <= 500; n +=2) // Iterate from 0 to 500 in steps of 2
 	{
-		printf("%d", n);
+		printf("%d", n); // Print the current value of n
 	}
 	return 0;
 }
