fff98f22a45cb28f3b674dd8d9b249b1ca0cc247
marky376 <mmutuamark@gmail.com>
2024-02-03 16:02:35 +0300

    Adding comments on files

--- 01calculator.c/grading2.c
+++ 01calculator.c/grading2.c
@@ -7,23 +7,23 @@ int main()
 	printf("Enter marks: ");
 	scanf("%d", &n);
 
-	if (n >= 70)
+	if (n >= 70) // If the marks are greater than or equal to 70
 	{
 		printf("This is a success\n");
 	}
-	else if (n >= 60)
+	else if (n >= 60) // If the marks are greater than or equal to 60
 	{
 		printf("This is above average\n");
 	}
-	else if (n >= 50)
+	else if (n >= 50) // If the marks are greater than or equal to 50
 	{
 		printf("This is an average result\n");
 	}
-	else if (n >= 40)
+	else if (n >= 40) // If the marks are greater than or equal to 40
 	{
 		printf("This is a satisfactory result\n");
 	}
-	else (n < 40)
+	else // If the marks are less than 40
 	{
 		printf("This is a PASS\n");
 	}
