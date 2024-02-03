66772a945c4242838962c4a6701580db7fee3aed
marky376 <mmutuamark@gmail.com>
2024-02-03 15:59:43 +0300

    Adding comments on files

--- 01calculator.c/forloop.c
+++ 01calculator.c/forloop.c
@@ -7,15 +7,14 @@ int main()
 
     k = 6;
 
-    for (i = 1; i <= k; i++)
+    for (i = 1; i <= k; i++) // Loop for rows
     {
-        for (b = 1; b <= i; b++)
+        for (b = 1; b <= i; b++) // Loop for columns
         {
-            printf("%c\n", '*');
+            printf("%c\n", '*'); // Print asterisk
         }
-	j++;
+        j++; // Increment asterisk character
     }
 
     return 0;
-
 }
