a6399129a919e3debc8ef485eb9c49998a5a7284
marky376 <mmutuamark@gmail.com>
2024-02-03 16:25:18 +0300

    Adding comments on files

--- 01calculator.c/tutlec.c
+++ 01calculator.c/tutlec.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 // variable declaration:
-extern int a, b;
-extern int c;
-extern float f;
+extern int a, b; // declare variables a and b
+extern int c; // declare variable c
+extern float f; // declare variable f
 
 int main()
 {
 	/* variable definition*/
-	int a, b;
-	int c;
-	float f;
+	int a, b; // define variables a and b
+	int c; // define variable c
+	float f; // define variable f
 	/* actual initialization*/
 
-	a = 10;
-	b = 20;
-	c = a + b;
+	a = 10; // initialize variable a with value 10
+	b = 20; // initialize variable b with value 20
+	c = a + b; // calculate the sum of a and b and assign it to c
 
-	printf("Value of c: %d/n", c);
-	f = 70.0 / 3.0;
+	printf("Value of c: %d/n", c); // print the value of c
+	f = 70.0 / 3.0; // calculate the division of 70.0 by 3.0 and assign it to f
 
-	printf("Value of f: %f\n", f);
+	printf("Value of f: %f\n", f); // print the value of f
 	return 0;
 }
