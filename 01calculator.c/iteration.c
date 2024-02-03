0bb6f1c0fa5e5b7c080952229baf6b8b00d3af2d
marky376 <mmutuamark@gmail.com>
2024-02-03 16:03:45 +0300

    Adding comments on files

--- 01calculator.c/iteration.c
+++ 01calculator.c/iteration.c
@@ -2,7 +2,7 @@
 
 void print_char(char c)
 {
-	write(1, &c, 1);
+	write(1, &c, 1); // writes the character 'c' to the standard output
 }
 
 void print_alphabet(void)
@@ -12,15 +12,15 @@ void print_alphabet(void)
 	c = 'a';
 	while (c <= 'z')
 	{
-		print_char(c);
-		c = c + 1;
+		print_char(c); // prints the current character
+		c = c + 1; // increments the character by 1
 	}
 }
 
 int main(void)
 {
-	print_alphabet();
-	print_char('\n');
+	print_alphabet(); // calls the function to print the alphabet
+	print_char('\n'); // prints a new line character
 
-	return (0);
+	return (0); // returns 0 to indicate successful execution
 }
