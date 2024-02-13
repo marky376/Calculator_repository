ae90632c3ab8294143df1cf0b9c7a39c8b243f40
marky376 <mmutuamark@gmail.com>
2024-02-13 12:26:14 +0300

    Sorting issues

---
+++ 01calculator.c/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char name[50];
+	cout << "Please enter your name: " ;
+	cin >> name;
+	
+	cout <<"Hello,there happy coding!!" ;
+
+	cout
+	cout <<"\n";
+	
+	return 0;
+}
+
