aea528e13f1bc6633814a8a4c3429af45088fc38
marky376 <mmutuamark@gmail.com>
2024-02-15 14:16:06 +0300

    A commit on files to work on

---
+++ 01calculator.c/class1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Room
+{
+    private:
+        double length;
+        double breadth;
+        double height;
+
+    public:
+        void intData(double len, double brth, double hgt)
+        {
+            length = len;
+            breadth = brth;
+            height = hgt;
+        }
+
+        double
+}
