ed86711c16c46e6ef7965185eecdb4ded83ccee0
marky376 <mmutuamark@gmail.com>
2024-02-15 14:22:18 +0300

    A commit on files to work on

--- 01calculator.c/class1.cpp
+++ 01calculator.c/class1.cpp
@@ -29,7 +29,7 @@ class Room
 int main()
 {
     Room room1;
-    room1.intData(42.5,30.8, 19.2)
+    room1.intData(42.5,30.8, 19.2);
     cout << "Area of room = "<<room1.calculateArea()<< endl;
     cout << "Volume of the room = "<<room1.calculateVolume() << endl;
 
