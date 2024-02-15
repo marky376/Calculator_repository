4bf80c3442ef6f4021c401ede4907faa966cadf6
marky376 <mmutuamark@gmail.com>
2024-02-15 18:59:15 +0300

    A commit on files to work on

---
+++ 01calculator.c/class2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Implement a class called employee that has private member variables name, employeeId and salary. Includde member functions to calculate and set SALARY based on employees performance
+class Employee
+{
+    private:
+        string name;
+        int employeeId;
+        double salary;
+    public:
+        void setEmployeeDetails(string n, int id, double s)
+        {
+            name = n;
+            employeeId = id;
+            salary = s;
+        }
+        void calculateSalary(double performance)
+        {
+            salary = salary + (salary * performance);
+        }
+        void displayEmployeeDetails()
+        {
+            cout << "Name: " << name << endl;
+            cout << "Employee ID: " << employeeId << endl;
+            cout << "Salary: " << salary << endl;
+        }
+};
+
