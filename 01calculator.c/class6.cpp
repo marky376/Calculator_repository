c8153040a319a06ac1f309d1a1844124ddcbaa02
marky376 <mmutuamark@gmail.com>
2024-03-05 07:14:05 +0300

    Somechanges

--- 01calculator.c/class6.cpp
+++ 01calculator.c/class6.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+using namespace std;
+
+class Spheres
+{
+	private:
+		double radius;
+	
+
+	public:
+		Sphere(double r)
+		{
+			radius = r;
+		}
+
+		double getVolume()
+		{
+			return ( 4/3 * 3.14 * radius * radius * radius);
+		}
+
+};
+
