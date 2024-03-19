0bbb04ec0b0f6dd56c0d109ac46dfb48d8f4cf5f
marky376 <mmutuamark@gmail.com>
2024-03-19 09:35:27 +0300

    Acommit on some files to work on with c++

--- 01calculator.c/cout.cpp
+++ 01calculator.c/cout.cpp
@@ -4,7 +4,7 @@ using namespace std; // A tool for avoiding the use of ::
 
 int main(void) //The Main function 
 {
-	cout <<"I enjoy prgramming"<<endl ;
+	cout <<"I enjoy prgramming"<<endl ; // The 
 	
 	return 0;
 }
