70ae1d4c1dd4487b871a370f9f5b78a12b8b94a1
marky376 <mmutuamark@gmail.com>
2024-03-19 13:13:28 +0300

    Acommit on some files to work on with c++

--- 01calculator.c/kangaru.cpp
+++ 01calculator.c/kangaru.cpp
@@ -2,3 +2,24 @@
 
 using namespace std;
 
+class Student
+{
+	private:
+		double regNo;
+
+	public:
+		char[100] Name;
+		int phoneNumber;
+		int age;
+
+		double getregNo()
+		{
+			cout << "What is your name? " << endl;
+			cin >> Name;
+
+			cout
+		}
+
+		
+}
+
